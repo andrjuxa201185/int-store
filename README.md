ccd1e4274794405bedf8a6628505ad9aaacd1668
andrey podgorniy <andrjuxa201185@gmai.com>
2018-10-11 22:49:45 +0300

    add readme

--- README.md
+++ README.md
@@ -7,3 +7,11 @@ git clone https://github.com/andrjuxa201185/int-store.git
 ## Upload npm packages:
 
 npm install
+
+### TO DO:
+
+Каждый кодит свой 'part.html' и '_style.scss'. 
+Все переменные по стилям кидаем в папку "_variables.scss". 
+Изображения кидаем в "images".
+
+Папку "build" не трогаем, она создается автоматом командой ""
--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
 const rigger = require('gulp-rigger');
- 
+const runSequence = require('run-sequence');
 
 
 
@@ -17,4 +17,10 @@ gulp.task('sass', function () {
      .pipe(sourcemaps.init())
      .pipe(sass().on('error', sass.logError))
      .pipe(gulp.dest('./app/styles/'));
-   });
+});
+
+gulp.task('run', function(){
+    runSequence('html', 'sass');
+})
+
+gulp.task('default', ['run'])   
