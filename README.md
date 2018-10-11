9dbc9b9a716c21270ee77ebd69fb69f266cc15b4
andrey podgorniy <andrjuxa201185@gmai.com>
2018-10-11 20:14:26 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# instruction:
+
+## Clone the project:
+
+git clone https://github.com/andrjuxa201185/internet-store.git
+
+## Upload npm packages:
+
+npm install
---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const rigger = require('gulp-rigger');
+ 
+
+
+
+gulp.task('html', function () {
+    gulp.src('app/components/*html')
+        .pipe(rigger())
+        .pipe(gulp.dest('build/'));
+});
+
+gulp.task('sass', function () {
+    return gulp.src('./style/style.scss')
+     .pipe(sourcemaps.init())
+     .pipe(sass().on('error', sass.logError))
+     .pipe(gulp.dest('./style'));
+   });
