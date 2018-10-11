9c0e5ce8ea98215b8ab923a66319674a9808d981
andrey podgorniy <andrjuxa201185@gmai.com>
2018-10-11 23:58:55 +0300

    add watchers

--- gulpfile.js
+++ gulpfile.js
@@ -3,29 +3,65 @@ const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
 const rigger = require('gulp-rigger');
 const runSequence = require('run-sequence');
-
+const watch = require('gulp-watch');
+const clean = require('gulp-clean');
+const browserSync = require('browser-sync');
+const reload = browserSync.reload;
 
 
 gulp.task('html', function () {
     gulp.src('app/components/*html')
         .pipe(rigger())
-        .pipe(gulp.dest('build/'));
+        .pipe(gulp.dest('build/'))
+        .pipe(reload({stream: true}));
 });
+
+gulp.task('clean', function(){
+    return gulp.src("build").pipe(clean());
+})
 
 gulp.task('sass', function () {
     return gulp.src('./app/styles/css.scss')
      .pipe(sourcemaps.init())
      .pipe(sass().on('error', sass.logError))
-     .pipe(gulp.dest('./app/styles/'));
+     .pipe(gulp.dest('./app/styles/'))
+     .pipe(reload({stream: true}));
 });
+
+gulp.task('images', function(){
+    return gulp.src("./app/images/**/*")
+    .pipe(gulp.dest('build/images/'))
+    .pipe(reload({stream: true}));
+})
 
 gulp.task('css', function(){
     return gulp.src("./app/styles/css.css")
     .pipe(gulp.dest('build/css/'))
+    .pipe(reload({stream: true}));
+})
+
+gulp.task('reload-css', function(){
+    runSequence('sass', 'css');
+})
+
+gulp.task('browser-sync', function(){
+    browserSync({
+        startPath: '/',
+        server: {
+            baseDir: 'build'
+        },
+        notify: false
+    })
+})
+
+gulp.task('watch',function(){
+    gulp.watch('app/components/**/*', ['html']);
+    gulp.watch('app/styles/*.scss', ['reload-css']);
+    gulp.watch('app/images/**/*', ['images']);
 })
 
 gulp.task('run', function(){
-    runSequence('html', 'sass', 'css');
+    runSequence('images', 'html', 'reload-css', 'browser-sync', 'watch');
 })
 
 gulp.task('default', ['run'])   
